9edf31997075219f7235b9fb0a09704b105250de
adamdawkins <adam@dragondrop.uk>
2020-02-11 21:18:47 +0000

    Make some initial tweaks

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,11 +1,3 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
-fi
-
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
@@ -14,11 +6,15 @@ alias gp='git push origin HEAD'
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
+alias ga='git add'
+alias gac='git add -A && git commit -m'
+alias gb='git branch'
 alias gc='git commit'
 alias gca='git commit -a'
-alias gco='git checkout'
 alias gcb='git copy-branch-name'
-alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gac='git add -A && git commit -m'
+alias gco='git checkout'
 alias ge='git-edit-new'
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias s='gs'
+
+alias theytookthelotmate='git push -u origin --all --follow-tags'
---
+++ git/functions.zsh
@@ -0,0 +1,7 @@
+function git-remote-merged () {
+  for branch in `git branch -r --merged | grep -v HEAD`; do echo -e `git show --format="%ci %cr %an" $branch | head -n 1` \\t$branch; done | sort -r
+}
+
+function git-remote-not-merged () {
+  for branch in `git branch -r --no-merged | grep -v HEAD`; do echo -e `git show --format="%ci %cr %an" $branch | head -n 1` \\t$branch; done | sort -r
+}
