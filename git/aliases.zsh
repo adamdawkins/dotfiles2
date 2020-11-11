32a557cce741cdf3073643201f502915becd4333
adamdawkins <adam@dragondrop.uk>
2020-11-11 15:12:34 +0000

    Add git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -24,3 +24,11 @@ alias release='git flow release'
 alias hotfix='git flow hotfix'
 
 alias theytookthelotmate='git push -u origin --all --follow-tags'
+
+function git-remote-merged () {
+  for branch in `git branch -r --merged | grep -v HEAD`; do echo -e `git show --format="%ci %cr %an" $branch | head -n 1` \\t$branch; done | sort -r
+}
+
+function git-remote-not-merged () {
+  for branch in `git branch -r --no-merged | grep -v HEAD`; do echo -e `git show --format="%ci %cr %an" $branch | head -n 1` \\t$branch; done | sort -r
+}
