803d4dd111ec0ccba81dd2a204f31c63363e6110
adamdawkins <adam@dragondrop.uk>
2022-11-09 17:09:36 +0000

    Updates, including git aliash completions

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,22 +1,31 @@
 alias g='git'
+compdef g=git
 alias gl='git pull --prune'
+compdef _git gl=git-pull
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+compdef _git gl=git-log
 alias gp='git push origin HEAD'
-
+compdef _git gp=git-push
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
+compdef _git gd=git-diff
 
 alias ga='git add'
-alias gac='git add -A && git commit -m'
+compdef _git ga=git-add
 alias gb='git branch'
+compdef _git gb=git-branch
 alias gc='git commit'
+compdef _git gc=git-commit
 alias gca='git commit -a'
-alias gcb='git copy-branch-name'
+compdef _git gca=git-commit
 alias gco='git checkout'
-alias ge='git-edit-new'
+compdef _git gco=git-checkout
 alias grh="git reset HEAD"
+compdef _git gco=git-reset
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+compdef _git gs=git-status
 alias s='gs'
+compdef _git s=git-status
 
 # git flow
 alias feature='git flow feature'
